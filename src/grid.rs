2f484ca1d153b2d49c23a5c99e21a1a5d47dc4a0
Peter Gagliardi <ptrgags@gmail.com>
2022-05-21 09:29:18 -0400

    Generate PNG image for maze data

--- src/grid.rs
+++ src/grid.rs
@@ -5,6 +5,9 @@ use crate::direction::Direction;
 
 const CELL_COUNT: usize = GRID_SIZE as usize * GRID_SIZE as usize;
 
+// RGB image
+const IMAGE_SIZE: usize = CELL_COUNT * 3;
+
 #[derive(Copy, Clone)]
 pub struct Connection {
     connected: bool,
@@ -41,10 +44,27 @@ impl Cell {
     }
 
     pub fn get_connection_bits(&self) -> u8 {
-        let up = self.connections[Direction::Up.to_index()].connected as u8;
-        let down = self.connections[Direction::Down.to_index()].connected as u8;
-        let left = self.connections[Direction::Left.to_index()].connected as u8;
-        let right = self.connections[Direction::Right.to_index()].connected as u8;
+        let up = self.connections[Direction::Up.to_index()]
+            .connected as u8;
+        let down = self.connections[Direction::Down.to_index()]
+            .connected as u8;
+        let left = self.connections[Direction::Left.to_index()]
+            .connected as u8;
+        let right = self.connections[Direction::Right.to_index()]
+            .connected as u8;
+        
+        up | (down << 1) | (left << 2) | (right << 3)
+    }
+
+    pub fn get_solution_bits(&self) -> u8 {
+        let up = self.connections[Direction::Up.to_index()]
+            .is_solution_connection as u8;
+        let down = self.connections[Direction::Down.to_index()]
+            .is_solution_connection as u8;
+        let left = self.connections[Direction::Left.to_index()]
+            .is_solution_connection as u8;
+        let right = self.connections[Direction::Right.to_index()]
+            .is_solution_connection as u8;
         
         up | (down << 1) | (left << 2) | (right << 3)
     }
@@ -80,6 +100,35 @@ impl Grid {
 
         self.get_cell_mut(a).connections[direction.to_index()].connected = true;
         self.get_cell_mut(b).connections[opposite_dir.to_index()].connected = true;
+    }
+
+    pub fn to_image_bytes(&self) -> [u8; IMAGE_SIZE] {
+        let mut result = [0; IMAGE_SIZE];
+        for i in 0..GRID_SIZE {
+            let row = (GRID_SIZE - 1) - i;
+            for j in 0..GRID_SIZE {
+                let index = row * GRID_SIZE + j;
+                let cell = &self.cells[index];
+                // Red channel is the connection bits
+                result[3 * index] = cell.get_connection_bits();
+                // Blue channel is the solution bits
+                result[3 * index + 1] = cell.get_solution_bits();
+                // Green channel was already initialized to 0
+            }
+        }
+        
+        result
+    }
+
+    pub fn to_debug_image_bytes(&self) -> [u8; IMAGE_SIZE] {
+        let mut image_bytes = self.to_image_bytes();
+        for i in 0..image_bytes.len() {
+            // Increase the contrast by shifting the 4 connection bits
+            // into the 4 high bits of each byte.
+            // For the green channel, 0 << 4 == 0 so this is a safe operation.
+            image_bytes[i] <<= 4;
+        }
+        image_bytes
     }
 
     pub fn mark_boundaries(&mut self) {
@@ -128,9 +177,9 @@ const GRID_CHARACTERS: [char; 16] = [
 impl Debug for Grid {
     fn fmt(&self, f: &mut Formatter) -> Result {
         for i in 0..GRID_SIZE {
+            let row = (GRID_SIZE - 1) - i;
             for j in 0..GRID_SIZE {
-                let row = (GRID_SIZE - 1) - i;
-                let index = row as usize * GRID_SIZE as usize + j as usize;
+                let index = row * GRID_SIZE + j;
                 let connection_bits = 
                     self.cells[index].get_connection_bits();
                 let grid_char = GRID_CHARACTERS[connection_bits as usize];
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod grid_coords;
 
 use crate::grid::Grid;
 use crate::dfs::DFSMaze;
+use crate::grid_coords::GRID_SIZE;
 
 fn main() {
     let mut grid = Grid::new();
@@ -12,6 +13,22 @@ fn main() {
 
     let mut maze_gen = DFSMaze::new();
     maze_gen.maze_fill(&mut grid);
+
+    image::save_buffer(
+        "output/grid.png", 
+        &grid.to_image_bytes(),
+        GRID_SIZE as u32, 
+        GRID_SIZE as u32,
+        image::ColorType::Rgb8
+    ).unwrap();
+
+    image::save_buffer(
+        "output/grid_debug.png", 
+        &grid.to_debug_image_bytes(),
+        GRID_SIZE as u32,
+        GRID_SIZE as u32,
+        image::ColorType::Rgb8
+    ).unwrap();
 
     println!("{:?}", grid);
 }
